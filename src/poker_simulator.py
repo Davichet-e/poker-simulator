2ac487f43837e55651d85ce0a0e0b96b1e7393a6
David Garcia <davidgmorillop@gmail.com>
2021-07-24 22:23:47 +0200

    ⚒ Continued work, turned into a module
    Started to create the CLI, also refactored the ClassicPoker class

--- src/poker_simulator.py
+++ src/poker_simulator.py
@@ -1,7 +1,11 @@
 """A simple poker simulator."""
 
-from abc import ABC, abs
-from deck import Deck
+from abc import ABC, abstractmethod
+
+from colorama import Fore, Style, deinit, init
+
+from .deck import Deck
+from .hand import Hand
 
 
 class Poker(ABC):
@@ -13,25 +17,33 @@ class Poker(ABC):
     def resolve(self) -> None:
         ...
 
-    @abstractmethod
-    def play(self) -> None:
-        ...
-
 
 class ClassicPoker(Poker):
     def __init__(self, n_players: int) -> None:
         self.n_players = n_players
-        self.deck = ...  # TODO
+        self.deck = Deck()
         self.hands = []
-        self.deal()
 
     def deal(self) -> None:
         for i in range(self.n_players):
-            hand = [deck.deal_card() for _ in range(5)]
-            self.hands.append()
+            hand = Hand(self.deck)
+            print(f"The hand of player {i + 1} is:")
+            print(hand)
+            print(hand.play)
+            self.hands.append(hand)
 
-    def resolve(self) -> None:
-        ...
+    def resolve(self) -> Hand:
+        return max(self.hands, key=lambda hand: hand.play)
 
-    def play(self) -> None:
-        ...
+
+if __name__ == "__main__":
+    init(autoreset=True)
+    print(f"Be welcomed to {Fore.GREEN}Poker Simulator{Style.RESET_ALL}.")
+    n_players = 3
+    print(f"Nº of selected players: {n_players}")
+    poker = ClassicPoker(n_players)
+    poker.deal()
+    print(poker.resolve())
+    print("test 2")
+    deinit()
+
